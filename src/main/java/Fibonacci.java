92722e3f7aff74187a46d87f03b7a8fbfdb7575b
Dahnia B. <belizairedahnia@gmail.com>
2025-05-11 10:16:20 -0400

    Initial commit to Main Branch

---
+++ src/main/java/Fibonacci.java
@@ -0,0 +1,19 @@
+public class Fibonacci {
+
+    /**
+     * Returns the nth Fibonacci number using recursion.
+     *
+     * @param n the position in the Fibonacci sequence
+     * @return the nth Fibonacci number
+     */
+    public static int fibonacci(int n) {
+        if (n <= 1) return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        int result = fibonacci(n);
+        System.out.println("The " + n + "th term of the Fibonacci sequence is " + result + ".");
+    }
+}
