851f9fd1aab3ac4cbf8a5ad1150c309f28f6512d
Dahnia B. <belizairedahnia@gmail.com>
2025-05-11 10:23:00 -0400

    Update Fibonacci.java

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,11 +1,6 @@
 public class Fibonacci {
 
-    /**
-     * Returns the nth Fibonacci number using recursion.
-     *
-     * @param n the position in the Fibonacci sequence
-     * @return the nth Fibonacci number
-     */
+
     public static int fibonacci(int n) {
         if (n <= 1) return n;
         return fibonacci(n - 1) + fibonacci(n - 2);
